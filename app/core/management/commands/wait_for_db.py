541fa401f000d0b7c63fed7fca8d287d1a2dc6f4
Valerii <v.bill2003@gmail.com>
2024-07-23 16:31:51 +0300

    Some local problem with db

--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -9,11 +9,25 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         self.stdout.write('Waiting for database...')
         db_up = False
+        attempt = 1
         while db_up is False:
             try:
                 self.check(databases=['default'])
                 db_up = True
-            except (Psycopg2OpError, OperationalError):
-                self.stdout.write('Database down, waiting 20 seconds...')
+                self.stdout.write(self.style.SUCCESS('Database available!'))
+            except (Psycopg2OpError, OperationalError) as e:
+                self.stdout.write(f'Attempt {attempt}: Database down, waiting 20 seconds...')
+                attempt += 1
                 time.sleep(20)
-        self.stdout.write(self.style.SUCCESS('Database available!'))
+                self.stdout.write(f'Error: {str(e)}')
+    # def handle(self, *args, **options):
+    #     self.stdout.write('Waiting for database...')
+    #     db_up = False
+    #     while db_up is False:
+    #         try:
+    #             self.check(databases=['default'])
+    #             db_up = True
+    #         except (Psycopg2OpError, OperationalError):
+    #             self.stdout.write('Database down, waiting 20 seconds...')
+    #             time.sleep(20)
+    #     self.stdout.write(self.style.SUCCESS('Database available!'))
